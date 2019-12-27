4564d694d095277a11d0e51057705371d771b3be
martyna1308 <martyna1308@gmail.com>
2019-12-27 15:10:14 +0100

    s

--- projekt4/build/js/main.js
+++ projekt4/build/js/main.js
@@ -6,8 +6,6 @@ Array.from(document.querySelectorAll(".menu-item")).forEach( (e) => {
 })
 
 
-
-
 document.querySelector(".menu-bars").addEventListener("click", () => {
     document.querySelector(".menu-mobile-bg").classList.toggle("menu-open");
 
@@ -19,13 +17,21 @@ document.querySelector(".menu-bars").addEventListener("click", () => {
 
 // FAQ
 let items = Array.from(document.getElementsByClassName("faq-item"));
-
 let questions = Array.from(document.getElementsByClassName("faq-question"));
 let answears = Array.from(document.getElementsByClassName("faq-answear"));
 let containers = Array.from(document.getElementsByClassName("faq-text-container"));
 
+
+containers.forEach( (g) => {
+    g.classList.remove("faq-text-container-anim");
+});
+
 questions.forEach( (e) => {
     e.addEventListener( "click", (e) => {
+        
+        containers.forEach( (g) => {
+            g.classList.remove("faq-text-container-anim");
+        });
 
         answears.forEach( (d) => {
             d.style.display = "none";
@@ -44,7 +50,8 @@ questions.forEach( (e) => {
                 
                 questions[i].style.fontWeight = 600;
                 answears[i].style.display = "inline";
-                containers[i].style.opacity = 1;
+                //containers[i].style.opacity = 1;
+                containers[i].classList.add("faq-text-container-anim")
                 
             }
         }
@@ -60,11 +67,29 @@ document.querySelector(".footer-line").addEventListener("click", () => {
     window.scrollTo({top: 0, behavior: 'smooth'});
 })
 
+// SHOW SECTIONS - HOME
+
+
+//home
+let header = document.getElementById("header");
+let footer = document.getElementById("footer");
+
+//header show
+window.addEventListener("DOMContentLoaded", () => {
+    setTimeout( () => {
+        header.style.opacity = 1;
+    }, 500);
     
+})
 
+//common elements show on scroll
+window.addEventListener("scroll", () => {
+    let actualScroll = window.pageYOffset;
 
-
-
-
+    if (actualScroll > footer.offsetTop - 600 ) { 
+        footer.style.opacity = 1;
+    }
+    
+})
 
 
