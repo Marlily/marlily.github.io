4f23de0ba5102fafa16ac986b412576fcbfe6a0e
martyna1308 <martyna1308@gmail.com>
2020-07-06 16:35:10 +0200

    meta

--- 5/build/js/main.js
+++ 5/build/js/main.js
@@ -1,34 +1,10 @@
-const menu = document.querySelector(".menu-open");
-console.log(menu);
-const buttonClose = document.querySelector(".menu-button-close");
-const buttonOpen = document.querySelector(".menu-button");
+//SCROLL TO TOP
 
-buttonOpen.addEventListener("click", function() {
-    menu.style.display = "flex";
-});
-
-buttonClose.addEventListener("click", function() {
-    menu.style.display = "none";
-});
+document.querySelector(".copyright button").addEventListener("click", () => {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+})
 
 
-//scroll buttons
-const buttonDown = document.getElementById("block-arrow-down");
-const buttonUp = document.getElementById("block-arrow-up");
-let footerTop = document.querySelector("#footer").offsetTop;
-let navTop = document.querySelector("#nav").offsetTop;
 
-buttonDown.addEventListener("click", function() {
 
-    window.scroll({
-        top: footerTop, 
-        behavior: 'smooth'
-      });
-});
 
-buttonUp.addEventListener("click", function() {
-    window.scroll({
-        top: navTop, 
-        behavior: 'smooth'
-      });
-});
