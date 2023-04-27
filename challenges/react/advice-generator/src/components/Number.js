457070cb5e686f3974c40ba0c14d28cd07caa27e
Martyna Banaś <banas.martynaa@gmail.com>
2023-04-27 17:00:30 +0200

    clean

--- challenges/react/advice-generator/src/components/Number.js
+++
@@ -1,7 +0,0 @@
-const Number = (props) => {
-    return (
-            <h1 className="advice-num">Advice #<span style={ props.visibility ? {opacity: 1} : {opacity: 0} }>{props.countAdvice}</span></h1>
-    );
-}
-
-export default Number;
